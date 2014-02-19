dda4c8611ab6069fbec02141b4b4f699295867c6
timothymiko <timothymiko@gmail.com>
2014-02-19 09:51:03 -0500

    integrated parse sdk
    Signed-off-by: timothymiko <timothymiko@gmail.com>

--- ProjectBearBus/src/com/bearbus/MainActivity.java
+++ ProjectBearBus/src/com/bearbus/MainActivity.java
@@ -1,15 +1,23 @@
 package com.bearbus;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
+import com.parse.Parse;
+import com.parse.PushService;
+
 public class MainActivity extends Activity {
+
+	private final String PARSE_APP_ID = "Hr5DPwQzhmzzST1sNzME8ssu3zaDxRZgtLO10Zxk";
+	private final String PARSE_CLIENT_KEY = "49AgCaNyWzaFFCgHFPgS3NK0lEjTpLNPDDYBrswX";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Parse.initialize(this, PARSE_APP_ID, PARSE_APP_ID);
+		PushService.setDefaultPushCallback(this, MainActivity.class);
 	}
 
 	@Override
